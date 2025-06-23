f7f18a79c4175bd1b742168b2cd6bf9a705df5d2
Yash Patel <yashnilay@gmail.com>
2025-06-23 02:02:20 -0400

    changes to yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yashnilay.ca',
+  site: 'yashnilay.ca',
   vite: { plugins: [tailwindcss()] },
   integrations: [react(), icon()], 
 });
